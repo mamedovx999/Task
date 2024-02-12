0a8e77d502926fcf53202ec04dd4a4887cfb9012
mamedovx999 <mammadalifm@code.edu.az>
2024-02-13 03:31:00 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#region Task1
+//int= 14
+////if (num==0)
+//{
+//    //  Console.WriteLine("duzgun eded daxil et")
+//}
+////else
+//{
+//    //if(num%2==0)
+//}
+////Console.WriteLine("cutdur");
+//}
+//else
+//{
+//Console.WriteLine("tekdir");
+//}
+
+
+#endregion
+#region Task2
+//int=21;
+// (n%3==0&&n&7==0)
+{
+   // Console.WriteLine("bolunur:);
+}
+//else
+{
+    //Console.WriteLine("bolunmur")
+}
+#endregion
+//#regionTask3
+//int n = 20;
+//int m = 22;
+//if (n + m % 2 == 0) 
+//{
+//    Console.WriteLine("n+m");
+//}
+//else
+//{
+//    Console.WriteLine("cut deyil");
+//}
+//#endregion
+#region Task4
+//int n = 10;
+//if (n >= 15) 
+//{
+//    Console.WriteLine("Hava istidir");
+//}
+//else
+//{
+//    Console.WriteLine("Hava soyugdur");
+//}
+
+
+#endregion
